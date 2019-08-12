2b71eb49d3703d1f07559b60ea20d20ce9ca047c
cuishihao <cuishihao>
2019-08-12 20:00:20 +0800

    push

--- src/main/java/com/csh/basic/ThreadTest.java
+++ src/main/java/com/csh/basic/ThreadTest.java
@@ -1,16 +1,37 @@
 package com.csh.basic;
 
 /**
- * @desc:
+ * @desc: 线程死锁测试
  * @author: CuiShiHao
  **/
 public class ThreadTest {
     public static void main(String[] args) {
-        Thread myThread = new Thread();
-        myThread.start();
-        myThread.interrupt();
-        System.out.println("第一次调用interrupt：" + Thread.interrupted());
-        System.out.println("第二次调用interrupt：" + Thread.interrupted());
-        System.out.println("END=========================================");
+        Thread thread1 = new Thread(() -> {
+            synchronized (String.class){
+                try {
+                    Thread.sleep(2000);
+                    synchronized (Integer.class){
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (Integer.class){
+                try {
+                    Thread.sleep(2000);
+                    synchronized (String.class){
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
     }
 }
