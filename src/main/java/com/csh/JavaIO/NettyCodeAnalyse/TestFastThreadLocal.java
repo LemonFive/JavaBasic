7321326ce28c3ec1e9728b5be8f982b353eee269
cuishihao <cuishihao>
2019-10-29 18:24:08 +0800

    Netty test

--- src/main/java/com/csh/JavaIO/NettyCodeAnalyse/TestFastThreadLocal.java
+++ src/main/java/com/csh/JavaIO/NettyCodeAnalyse/TestFastThreadLocal.java
@@ -6,7 +6,7 @@ import io.netty.util.concurrent.FastThreadLocal;
  * @desc: FastThreadLocal源码分析
  * @author: CuiShiHao
  **/
-public class FastThreadLocalTest {
+public class TestFastThreadLocal {
     private static FastThreadLocal<Object> threadLocal =
             new FastThreadLocal<Object>(){
         @Override
@@ -18,11 +18,12 @@ public class FastThreadLocalTest {
     private static Object object = new Object();
 
     public static void main(String[] args) {
-        ;
         new Thread(() ->{
             Object object = threadLocal.get();
             // ...do with object
             System.out.println(object);
+
+            threadLocal.set(new Object());
         }).start();
 
         new Thread(() ->{
