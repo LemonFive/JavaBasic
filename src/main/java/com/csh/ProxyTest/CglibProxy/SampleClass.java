e00ee54b3845340fe4ea0ac95294868ed1d4fed7
cuishihao <cuishihao>
2019-10-30 09:39:04 +0800

    cglib 代理

---
+++ src/main/java/com/csh/ProxyTest/CglibProxy/SampleClass.java
@@ -0,0 +1,11 @@
+package com.csh.ProxyTest.CglibProxy;
+
+/**
+ * @desc:
+ * @author: CuiShiHao
+ **/
+public class SampleClass {
+    public void test(){
+        System.out.println("hello world");
+    }
+}
